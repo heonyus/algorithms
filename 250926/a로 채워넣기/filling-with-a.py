21a76d4fdaf225c81595d9e6ea75944470e0f6de
Jay <108461006+heonyus@users.noreply.github.com>
2025-09-26 02:46:36 +0900

    Title: a로 채워넣기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250926/a로 채워넣기/filling-with-a.py
+++ 250926/a로 채워넣기/filling-with-a.py
@@ -1,6 +1,13 @@
 word = input()
 
-new_word = word.replace(word[1], "a", 1)
-new_word_1 = new_word.replace(word[-2], "a", 1)
+# new_word = word.replace(word[1], "a", 1)
+# new_word_1 = new_word.replace(word[-2], "a", 1)
 
-print(new_word_1)
+# print(new_word_1)
+
+word_list = list(word)
+word_list[1]= 'a'
+word_list[-2]= 'a'
+
+fin_word = "".join(word_list)
+print(fin_word)
