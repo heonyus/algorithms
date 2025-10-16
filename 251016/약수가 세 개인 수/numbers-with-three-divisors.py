7afb481cf3120a9e92810985872d29582491b1c2
Jaeh <108461006+heonyus@users.noreply.github.com>
2025-10-16 22:33:47 +0900

    Title: 약수가 세 개인 수, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251016/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,14 @@
+start, end = map(int, input().split())
+
+cnt = 0
+# Please write your code here.
+for i in range(start, end+1):
+    y = []
+    for j in range(1, i+1):
+        if i % j == 0:
+            y.append(j)
+    if len(y) == 3:
+        cnt += 1
+
+print(cnt)
+        
